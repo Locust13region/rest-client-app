c965aa6d17cf6bb18205e1da907de5eaff7366f3
ishoshina <irina.akhotina@gmail.com>
2025-09-06 16:40:52 +0300

    refactor: linter rules

--- app/[lang]/theme-provider.tsx
+++ app/[lang]/theme-provider.tsx
@@ -1,5 +1,5 @@
 'use client';
-
+import React from 'react';
 import { ThemeProvider, CssBaseline } from '@mui/material';
 import { AppRouterCacheProvider } from '@mui/material-nextjs/v15-appRouter';
 import theme from '@/theme';
