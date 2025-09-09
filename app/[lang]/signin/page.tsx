81b17bae12f38102531765434aedb7f60c9d1b93
FX506LL\masan <locust13region@gmail.com>
2025-09-09 17:10:12 +0300

    refactor: signin signup removed duplicate code

--- app/[lang]/signin/page.tsx
+++ app/[lang]/signin/page.tsx
@@ -1,25 +1,12 @@
 'use client';
 
 import { FormEvent, useEffect } from 'react';
-import Box from '@mui/material/Box';
-import Button from '@mui/material/Button';
-import Divider from '@mui/material/Divider';
-import FormLabel from '@mui/material/FormLabel';
-import FormControl from '@mui/material/FormControl';
-import TextField from '@mui/material/TextField';
-import Typography from '@mui/material/Typography';
 import { useSignInWithEmailAndPassword } from 'react-firebase-hooks/auth';
 import { auth } from '@/firebase/config';
-import { Card, SignContainer } from '@/style/styledSign';
 import { useRouter } from 'next/navigation';
-import { useTranslations } from 'next-intl';
-import MuiLink from '@mui/material/Link';
-import { Link } from '@/i18n/navigation';
 import SignForm from '@/components/sign-form/sign-form';
 
 export default function SignIn() {
-  const t = useTranslations('Sign');
-
   const [signInUser, result, loading, signInUserError] =
     useSignInWithEmailAndPassword(auth);
   const router = useRouter();
@@ -57,64 +44,6 @@ export default function SignIn() {
   }, [loading, signInUserError]);
 
   return (
-    <SignContainer direction="column" justifyContent="space-between">
-      <Card variant="outlined">
-        <Typography
-          component="h1"
-          variant="h4"
-          sx={{ width: '100%', fontSize: 'clamp(2rem, 10vw, 2.15rem)' }}
-        >
-          {t('signIn')}
-        </Typography>
-        <SignForm handleSubmit={handleSubmit}>
-          <FormControl>
-            <FormLabel htmlFor="email">{t('email')} </FormLabel>
-            <TextField
-              required
-              fullWidth
-              name="email"
-              placeholder="your@email.com"
-              type="email"
-              id="email"
-              autoComplete="email"
-              variant="outlined"
-            />
-          </FormControl>
-          <FormControl>
-            <FormLabel htmlFor="password">{t('password')} </FormLabel>
-            <TextField
-              required
-              fullWidth
-              name="password"
-              placeholder="••••••"
-              type="password"
-              id="password"
-              autoComplete="new-password"
-              variant="outlined"
-            />
-          </FormControl>
-          <Button type="submit" fullWidth variant="contained">
-            {t('signIn')}
-          </Button>
-        </SignForm>
-        <Divider>
-          <Typography sx={{ color: 'text.secondary' }}>{t('or')} </Typography>
-        </Divider>
-        <Box sx={{ display: 'flex', flexDirection: 'column', gap: 2 }}>
-          <Typography sx={{ textAlign: 'center' }}>
-            {t('alreadyHave')}{' '}
-            <Link href="/signup">
-              <MuiLink
-                component="span"
-                variant="body2"
-                sx={{ alignSelf: 'center' }}
-              >
-                {t('signUp')}
-              </MuiLink>
-            </Link>
-          </Typography>
-        </Box>
-      </Card>
-    </SignContainer>
+    <SignForm formType={'signIn'} required={true} handleSubmit={handleSubmit} />
   );
 }
--- app/[lang]/signup/page.tsx
+++ app/[lang]/signup/page.tsx
@@ -1,22 +1,11 @@
 'use client';
 
 import { FormEvent, useEffect, useState } from 'react';
-import Box from '@mui/material/Box';
-import Button from '@mui/material/Button';
-import Divider from '@mui/material/Divider';
-import FormLabel from '@mui/material/FormLabel';
-import FormControl from '@mui/material/FormControl';
-import TextField from '@mui/material/TextField';
-import Typography from '@mui/material/Typography';
 import { useCreateUserWithEmailAndPassword } from 'react-firebase-hooks/auth';
 import { auth } from '@/firebase/config';
-import { Card, SignContainer } from '@/style/styledSign';
 import z from 'zod';
 import { useFormSchema, type FormData } from './types';
 import { useRouter } from 'next/navigation';
-import { useTranslations } from 'next-intl';
-import MuiLink from '@mui/material/Link';
-import { Link } from '@/i18n/navigation';
 import SignForm from '@/components/sign-form/sign-form';
 
 const initialFormState = {
@@ -28,7 +17,6 @@ export default function SignUp() {
   const [userFormData, setFormData] = useState<FormData>(initialFormState);
   const [showErrors, setShowErrors] = useState(false);
   const formSchema = useFormSchema();
-  const t = useTranslations('Sign');
 
   const [createUser, result, loading, createUserError] =
     useCreateUserWithEmailAndPassword(auth);
@@ -80,80 +68,12 @@ export default function SignUp() {
   const errors = showErrors ? validate() : undefined;
 
   return (
-    <SignContainer direction="column" justifyContent="space-between">
-      <Card variant="outlined">
-        <Typography
-          component="h1"
-          variant="h4"
-          sx={{ width: '100%', fontSize: 'clamp(2rem, 10vw, 2.15rem)' }}
-        >
-          {t('signUp')}
-        </Typography>
-        <SignForm handleSubmit={handleSubmit}>
-          <FormControl>
-            <FormLabel htmlFor="email">{t('email')} </FormLabel>
-            <TextField
-              fullWidth
-              id="email"
-              placeholder="your@email.com"
-              name="email"
-              autoComplete="email"
-              variant="outlined"
-              value={userFormData.email}
-              onChange={(e) =>
-                setFormData((prev) => ({ ...prev, email: e.target.value }))
-              }
-              error={errors?.email?.length ? true : false}
-              helperText={errors?.email?.join(', ')}
-              color={(errors?.email?.length ?? 0) ? 'error' : 'primary'}
-            />
-          </FormControl>
-          <FormControl>
-            <FormLabel htmlFor="password">{t('password')} </FormLabel>
-            <TextField
-              fullWidth
-              name="password"
-              placeholder="••••••"
-              type="password"
-              id="password"
-              autoComplete="new-password"
-              variant="outlined"
-              value={userFormData.password}
-              onChange={(e) =>
-                setFormData((prev) => ({ ...prev, password: e.target.value }))
-              }
-              error={errors?.password?.length ? true : false}
-              helperText={errors?.password?.join(', ')}
-              color={(errors?.password?.length ?? 0) ? 'error' : 'primary'}
-            />
-          </FormControl>
-          <Button
-            type="submit"
-            fullWidth
-            variant="contained"
-            disabled={!!errors}
-          >
-            {t('signUp')}
-          </Button>
-        </SignForm>
-        <Divider>
-          <Typography sx={{ color: 'text.secondary' }}>{t('or')} </Typography>
-        </Divider>
-        <Box sx={{ display: 'flex', flexDirection: 'column', gap: 2 }}>
-          <Typography sx={{ textAlign: 'center' }}>
-            {t('alreadyHave')}{' '}
-            <Link href="/signin">
-              <MuiLink
-                component="span"
-                variant="body2"
-                sx={{ alignSelf: 'center' }}
-              >
-                {t('signIn')}
-              </MuiLink>
-            </Link>
-          </Typography>
-        </Box>
-      </Card>
-    </SignContainer>
+    <SignForm
+      formType={'signUp'}
+      userFormData={userFormData}
+      setFormData={setFormData}
+      handleSubmit={handleSubmit}
+      errors={errors}
+    />
   );
 }
