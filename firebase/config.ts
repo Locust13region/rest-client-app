70d322404c84b41761814100aade310296a3961a
DESKTOP-MF8V5D1\Andrey <m>
2025-09-17 13:33:37 +0300

    feat: init FB RTDB

--- firebase/config.ts
+++ firebase/config.ts
@@ -1,5 +1,6 @@
 import { getApp, getApps, initializeApp } from 'firebase/app';
 import { getAuth } from 'firebase/auth';
+// import { getDatabase } from 'firebase/database';
 
 const firebaseConfig = {
   apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
@@ -9,6 +10,7 @@ const firebaseConfig = {
   messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
   appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
   measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID,
+  databaseURL: 'https://DATABASE_NAME.firebaseio.com',
 };
 
 const app = !getApps().length ? initializeApp(firebaseConfig) : getApp();
