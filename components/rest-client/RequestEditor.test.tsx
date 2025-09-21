4c49d89628ca775db1064592f81dfc5f0f7df7d1
ishoshina <irina.akhotina@gmail.com>
2025-09-21 12:19:37 +0300

    feat: RequestEditor test cleanup

--- components/rest-client/RequestEditor.test.tsx
+++ components/rest-client/RequestEditor.test.tsx
@@ -6,18 +6,29 @@ import { beforeEach } from 'node:test';
 import {
   ReadonlyURLSearchParams,
   useParams,
+  useRouter,
   useSearchParams,
 } from 'next/navigation';
+import { AppRouterInstance } from 'next/dist/shared/lib/app-router-context.shared-runtime';
+
+const url = 'https://jsonplaceholder.typicode.com/posts';
+const mockRouter: AppRouterInstance = {
+  replace: vi.fn(),
+  back: vi.fn(),
+  forward: vi.fn(),
+  refresh: vi.fn(),
+  push: vi.fn(),
+  prefetch: vi.fn(),
+};
 
 vi.mock('next/navigation', async () => {
   const actual = await vi.importActual('next/navigation');
-
   return {
     __esModule: true,
     ...actual,
-    useParams: vi.fn().mockReturnValue({ slug: '' }),
-    useRouter: vi.fn(),
+    useParams: vi.fn(),
     useSearchParams: vi.fn(),
+    useRouter: vi.fn(),
   };
 });
 
@@ -30,9 +41,7 @@ describe('RequestEditor', () => {
       () => new URLSearchParams('') as ReadonlyURLSearchParams
     );
 
-    renderWithProviders(<RequestEditor onSend={handleSend} />, {
-      locale: 'en',
-    });
+    renderWithProviders(<RequestEditor onSend={handleSend} />);
 
     expect(screen.getByRole('combobox')).toHaveTextContent(/get/i);
     expect(screen.getByPlaceholderText(/enter URL/i)).toBeInTheDocument();
@@ -58,8 +67,6 @@ describe('RequestEditor', () => {
   it('restores settings from URL', async () => {
     const handleSend = vi.fn();
     const headers = 'Content-Type=application/json';
-    const url = 'https://jsonplaceholder.typicode.com/posts';
-    //const body = '{"title":"Hello World"}';
     vi.mocked(useParams).mockReturnValue({
       slug: [
         'POST',
@@ -71,12 +78,7 @@ describe('RequestEditor', () => {
       () => new URLSearchParams(headers) as ReadonlyURLSearchParams
     );
 
-    const { user } = renderWithProviders(
-      <RequestEditor onSend={handleSend} />,
-      {
-        locale: 'en',
-      }
-    );
+    const { user } = renderWithProviders(<RequestEditor onSend={handleSend} />);
 
     const headersTab = screen.getByRole('tab', { name: /headers/i });
     await user.click(headersTab);
@@ -89,34 +91,40 @@ describe('RequestEditor', () => {
       const vals = screen.getAllByPlaceholderText(/value/i);
       expect(keys[0]).toHaveValue(headers.split('=')[0]);
       expect(vals[0]).toHaveValue(headers.split('=')[1]);
-      //expect(within(bodyTab).getByRole('textbox')).toHaveValue(body);
     });
   });
   it('triggers settings updates', async () => {
     const handleSend = vi.fn();
-    const handleMethodChange = vi.fn();
+    const routerSpy = vi.fn();
     vi.mocked(useParams).mockReturnValue({ slug: '' });
     vi.mocked(useSearchParams).mockImplementation(
       () => new URLSearchParams('') as ReadonlyURLSearchParams
     );
+    mockRouter.replace = routerSpy;
+    vi.mocked(useRouter).mockImplementationOnce(() => mockRouter);
 
-    const { user } = renderWithProviders(
-      <RequestEditor onSend={handleSend} />,
-      {
-        locale: 'en',
-      }
-    );
-
+    const { user } = renderWithProviders(<RequestEditor onSend={handleSend} />);
+    let option: HTMLElement | null = null;
     const methodSelect = screen.getByRole('combobox');
-    //await user.click(methodSelect);
-    // await waitFor(() => {
-    //   const put = screen.getByRole('listitem', { name: /put/i });
-    //   expect(put).toBeInTheDocument();
-    // });
-    //await user.selectOptions(methodSelect, 'PUT');
-    await waitFor(() => {
-      expect(screen.getByRole('combobox')).toHaveTextContent(/get/i);
-      //expect(handleMethodChange).toBeCalled();
+    await user.click(methodSelect);
+    await waitFor(async () => {
+      const options = screen.getAllByRole('option');
+      expect(options).toHaveLength(5);
+      option = options[2];
+      expect(option).toHaveTextContent(/put/i);
+    });
+    if (option) {
+      await user.click(option);
+      await waitFor(async () => {
+        expect(screen.getByRole('combobox')).toHaveTextContent(/put/i);
+        expect(routerSpy).toHaveBeenCalled();
+      });
+    }
+    const urlInput = screen.getByPlaceholderText(/enter URL/i);
+    await user.type(urlInput, url);
+    await waitFor(async () => {
+      await user.tab();
+      expect(urlInput).toHaveValue(url);
     });
   });
 });
